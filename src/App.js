b93317e1c7abc7ed8c83edc59a7779b7c9739090
pwujczyk <pwujczyk@ecovadis.com>
2020-01-01 17:18:40 +0100

    first component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+
+
+
+const list=[
+	{
+		title:'React',
+		author:'React author',
+		objectId:1
+	},
+	{
+		title:'Node',
+		author:'Node author',
+		objectId:2
+	}
+]
 
 function App() {
   return (
     <div className="App">
-		pawel
+	{list.map(function(item){
+		return (
+		<div>
+			<span>{item.objectId} </span>
+			<span>{item.title}</span>
+			<Notes/>
+		</div>
+		);
+	})}
+	
     </div>
   );
+}
+
+class Notes extends Component{
+	
+	render(){
+		return (
+			<div>
+				<p>Type before/after</p>
+				<p><input type="text"></input></p>
+			</div>
+		)
+	}
 }
 
 export default App;
