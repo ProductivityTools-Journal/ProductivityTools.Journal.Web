656c5e3024f79f91482b2049a501904dda59c90c
pwujczyk <pwujczyk@ecovadis.com>
2020-03-15 14:21:47 +0100

    routing

--- src/App.js
+++ src/App.js
@@ -1,73 +1,36 @@
 import React, {Component} from 'react';
 import './App.css';
-import MeetingItem from 'Components/MeetingItem'
 
-
-
-//const PATH_BASE='http://productivitytools.tech:8081/api/	';
-const PATH_BASE='https://localhost:5001/api/';
-const PATH_MEETINGS_CONTROLER='Meetings';
-const PATH_MEETINGS_ACTION='List';
+import MeetingList from 'Components/MeetingList'
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+  } from "react-router-dom";
+import EditMeeting from 'Components/EditMeeting';
 
 
 class App extends Component {
-
-	constructor(props) {
-		super(props);
-		this.state={
-			meetings:[]
-		}
-
-		this.setMeetings=this.setMeetings.bind(this);
-	}	
-
-  	render (){	
-		const{meetings}=this.state;
-		//because render is before compnentDidMount
-		if(!meetings){return null}
-		
-	  	return(		
-    		<div className="App">
-			{this.state.meetings.map(function(item){
-				
-				return (
-				<div key={item.meetingId}>
-					<span>CXX</span>
-					<span>{item.meetingId} </span>
-					<span>{item.subject}</span>
-				
-					<MeetingItem meeting={item}/>
+	render (){	
+		return(
+			<div>
+				<div>
+					<Link to="/">List</Link>
+					<Link to="Edit">New</Link>
 				</div>
-				);
-			})}
-	
-   			 </div>
- 		);
-	}
-
-	setMeetings(meetings){
-		console.log(meetings)
-		this.setState({meetings});
-	}
-
-	componentDidMount(){
-
-
-		console.log("Post");
-		fetch(`${PATH_BASE}${PATH_MEETINGS_CONTROLER}/${PATH_MEETINGS_ACTION}`,{
-			mode: 'cors',
-			crossDomain:true,
-			method:'POST',
-			headers: {'Content-Type':'application/json'},
-			body: JSON.stringify("SS")
-	
-		})
-		.then(respone=>respone.json())
-		.then(result=>this.setMeetings(result))
-		.catch(error=>error);
-		console.log("Finish post");
+				<Switch>
+					<Route path="/Edit">
+						<EditMeeting/>
+					</Route>
+					<Route path="/">
+						<MeetingList/>
+					</Route>				
+				</Switch>	
+				
+			</div>			
+		)
 	}
 }
-
 
 export default App;
---
+++ src/Components/EditMeeting/index.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class EditMeeting extends Component{
+	
+	constructor(props){
+        super(props);
+
+	}
+	
+	render(){
+		return (
+        <p>pawel</p>
+		)
+	}
+}
+
+export default EditMeeting;
