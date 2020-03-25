495d15887ee9118c963df5cedc6808af0755e3c7
pwujczyk <pwujczyk@ecovadis.com>
2020-03-25 23:32:16 +0100

    home page and test call

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,16 +1,47 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
+import * as Consts from 'Consts'
+import * as moment from 'moment';
 
 class Home extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { date: null }
+    }
+
+    loadDate = (date) => {
+       
+        this.setState({ date });
+    }
+
+    componentDidMount() {
+        console.log("calling endpoint to get current date");
+        fetch(`${Consts.PATH_BASE}${Consts.PATH_MEETINGS_CONTROLER}/${Consts.PATH_MEETINGS_DATE}`, {
+            mode: 'cors',
+            crossDomain: true,
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' }
+        })
+            .then(respone => respone.json())
+            .then(result => this.loadDate(result))
+            .catch(error => error);
+        console.log("Finish post");
+    }
 
 
     render() {
         const { isAuthenticated, login, logout } = this.props.auth;
+        let mt = moment(this.state.date);
+        let dtFormated = mt.format('YYYY.MM.DD hh:mm:ss')
         return (
+
             <div>
+
                 <p>Welcome on home page of ProductivityTools.Meeting</p>
+                <p>{this.state.date ? `Server responded with date ${dtFormated}` : "Server hasn't responded yet"}</p>
                 {isAuthenticated() ? (<Link to="/List">List</Link>) : <p></p>}
-                <button onClick={isAuthenticated()?logout:login}>{isAuthenticated() ? "Log out" : "Log in"}</button>
+                <button onClick={isAuthenticated() ? logout : login}>{isAuthenticated() ? "Log out" : "Log in"}</button>
             </div >
         )
     }
--- src/Consts.js
+++ src/Consts.js
@@ -2,6 +2,7 @@
 export const PATH_BASE = 'https://localhost:5001/api/';
 export const PATH_MEETINGS_CONTROLER = 'Meetings';
 export const PATH_MEETINGS_ACTION = 'List';
+export const PATH_MEETINGS_DATE = 'Date';
 export const PATH_MEETING_ACTION = 'Meeting';
 export const PATH_MEETING_UPDATE_MEETING= 'Update';
 export const PATH_MEETING_NEW_MEETING= 'New';
