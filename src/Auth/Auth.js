9f3e17e72d18c5113b77dca13b824143cae0bc37
pwujczyk <pwujczyk@ecovadis.com>
2020-04-06 22:44:56 +0200

    missing audience for authorization

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -7,6 +7,7 @@ export default class Auth {
             domain: process.env.REACT_APP_AUTH0_DOMAIN,
             clientID: process.env.REACT_APP_AUTH0_CLIENTID,
             redirectUri: process.env.REACT_APP_AUTH0_CALLBACK_URL,
+            audience: process.env.REACT_APP_AUTH0_AUDIENCE,
             responseType: "token id_token",
             scope: "openid profile email"
         })
@@ -28,6 +29,7 @@ export default class Auth {
     }
 
     handleAuthentication = () => {
+        debugger;
         this.auth0.parseHash((err, authResult) => {
             if (authResult && authResult.accessToken && authResult.idToken) {
                 this.setSession(authResult);
