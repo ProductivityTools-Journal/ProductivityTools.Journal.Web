cc2e58cb04c8f7923029d0b9a40d51540b73467b
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-02-06 10:35:06 +0100

    move to different note element

--- src/Components/JournalItem/index.js
+++ src/Components/JournalItem/index.js
@@ -3,6 +3,8 @@ import AutoHideNotes from 'Components/AutoHideNotes'
 import Button from '@material-ui/core/Button';
 import { Link } from "react-router-dom";
 import * as moment from 'moment';
+import NotesLabel from 'Components/NotesLabel'
+
 
 
 function MeetingItem(props) {
@@ -12,10 +14,15 @@ function MeetingItem(props) {
 	let dtDescription = mt.fromNow();
 	let dtFormated = mt.format('YYYY.MM.DD hh:mm')
 	const buttonStyle = { textAlign: 'left' }
+	console.log("meeting");
+	console.log(props.meeting);
+
 	return (
 		<fieldset key={props.meeting.meetingId}>
 			<legend>[{props.meeting.meetingId}] {dtFormated} ({dtDescription}) - {props.meeting.subject} Treeid:{props.meeting.treeId}</legend>
-
+			{props.meeting.notes.map(n => {
+				return (<NotesLabel title={n.type} notes={n.notes} />)
+			})}
 			{/*<Button  variant="contained"  color="primary" onClick={()=>this.edit()}>Edit</Button>*/}
 			<AutoHideNotes title='Before notes' notes={props.meeting.beforeNotes} />
 			<AutoHideNotes title='During notes' notes={props.meeting.duringNotes} />
