58379a2f5975ac59f98382e0db3822f18d89616c
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-03-22 19:36:58 +0100

    update version, showing home

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
-import React, { Component } from 'react';
 import './App.css';
 
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+//import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
-import JournalItemEdit from 'Components/JournalItemEdit';
-//import NewMeeting from 'Components/NewItem';
+//import JournalItemEdit from 'Components/JournalItemEdit';
+// //import NewMeeting from 'Components/NewItem';
 import Home from 'Components/Home';
-import Main from 'Components/Main'
+//import Main from 'Components/Main'
 
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+//import { ToastContainer } from 'react-toastify';
+//import 'react-toastify/dist/ReactToastify.css';
 
 
-class App extends Component {
+function App() {
+	return (
+		<div className="App">
+			<Home/>
+		</div>)
+	// <div>
+	// 	<div>
+	// 		<Link to="/">Home1</Link>
+	// 		<Link to="/List">List</Link>
+	// 	</div>
+	// 	<Router>
+	// 		<Routes>
+	// 			<Route path="/Edit/:Id" render={(props) => (<JournalItemEdit {...props} key={this.props.Id} />)}></Route>
+	// 			<Route path="/List/:TreeId" render={(props => <Main auth={this.auth} {...props} />)}></Route>
+	// 			<Route path="/List/" exact render={(props => <Main auth={this.auth} {...props} />)}></Route>
+	// 			<Route path="/">
+	// 				<Home auth={this.auth} {...this.props} />
+	// 			</Route>
+	// 		</Routes>
+	// 	</Router>
+	// 	<ToastContainer />
+	// </div>
+	//)
 
-	render() {
-		return (
-			<div>
-				<div>
-					<Link to="/">Home1</Link>
-					<Link to="/List">List</Link>
-				</div>
-				<Router>
-					<Routes>
-						<Route path="/Edit/:Id" render={(props) => (<JournalItemEdit {...props} key={this.props.Id} />)}></Route>
-						<Route path="/List/:TreeId" render={(props => <Main auth={this.auth} {...props} />)}></Route>
-						<Route path="/List/" exact render={(props => <Main auth={this.auth} {...props} />)}></Route>
-						<Route path="/">
-							<Home auth={this.auth} {...this.props} />
-						</Route>
-					</Routes>
-				</Router>
-				<ToastContainer />
-			</div>
-		)
-	}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ import { BrowserRouter, Route } from "react-router-dom";
 
 
 ReactDOM.render(
-<BrowserRouter>
-    <Route path="/" component={App} />
-</BrowserRouter>, 
-document.getElementById('root'));
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
