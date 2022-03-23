143f4acf52a4d1642d86601ebdb09560cab29e2e
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-03-23 20:44:53 +0100

    ougout

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,47 +1,29 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
-import { AuthService } from '../../OAuth/OAuth';
 import * as moment from 'moment';
 import * as apiService from 'services/apiService'
+import {auth,logout} from '../../Session/firebase'
 
 export default function Home() {
 
 
-    let authService = new AuthService();
     const [date, setDate] = useState();
-    const [userAuthenticated, setUserAuthenticated] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
             const data = await apiService.getDate();
             setDate(data);
         }
-
-        authService.getUser().then(user => {
-            console.log("uu");
-            console.log(user);
-            if (user == null) {
-                setUserAuthenticated(false);
-            }
-            else {
-                setUserAuthenticated(true);
-            }
-            console.log(user);
-        })
         fetchData();
-    },[authService]);
+    },[]);
 
     const login = () => {
-        authService.login();
     }
 
-    const logout = () => {
-        authService.logout();
+    const buttonLogout = () => {
+        logout();
     }
 
-
-    console.log("User");
-    console.log(authService.getUser());
     let mt = moment(date);
     let dtFormated = mt.format('YYYY.MM.DD hh:mm:ss')
 
@@ -50,8 +32,9 @@ export default function Home() {
         <div>
             <p>Welcome on home page of ProductivityTools.Meeting :-)</p>
             <p>{date ? `Server responded with date ${dtFormated}` : "Server hasn't responded yet"}</p>
-            {userAuthenticated ? (<Link to="/List">List</Link>) : <p></p>}
-            <button onClick={userAuthenticated ? logout : login}>{userAuthenticated ? "Log out" : "Log in"}</button>
+            <p>{auth.currentUser.displayName}</p>
+            <p>{auth.currentUser.email} </p>
+            <button onClick={buttonLogout}>logout</button>
             <Link to="/List">List</Link>
         </div >
     )
