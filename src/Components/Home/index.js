a04f3059bfdf77d8a407c03200a1809079befe69
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-05-28 22:10:35 +0200

    journal

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -32,7 +32,7 @@ export default function Home() {
 
     return (
         <div>
-            <p>Welcome on home page of ProductivityTools.Meeting :-)</p>
+            <p>Welcome on home page of ProductivityTools.Journal</p>
             <p>{date ? `Server responded with date ${dtFormated}` : "Server hasn't responded yet"}</p>
             <p>{auth?.currentUser?.displayName}</p>
             <p>{auth?.currentUser?.email} </p>
