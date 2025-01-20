127903a7d86f864fa7c48b7c00b04173e7778061
Pawel Wujczyk <pwujczyk@gmail.com>
2025-01-20 19:57:08 +0100

    config

--- src/Consts.js
+++ src/Consts.js
@@ -30,7 +30,8 @@ const dev={
 
 const prod={
     clientId:"prodmeetingsweb",
-    PATH_BASE :'https://apijournal.productivitytools.top:8022/api/',
+    //PATH_BASE :'https://apijournal.productivitytools.top:8022/api/',
+    PATH_BASE :'https://journal-api.productivitytools.top/api/',
     stsAuthority : 'https://identityserver.productivitytools.top:8010/',
     clientRoot : 'https://meetingsweb.z13.web.core.windows.net/',
   //  apiRoot : 'http://localhost:5002/'
