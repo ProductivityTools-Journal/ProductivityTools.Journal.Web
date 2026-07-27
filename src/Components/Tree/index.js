239a8bed94dce11b836e64e5478db1e3f80fe413
Pawel Wujczyk <pwujczyk@gmail.com>
2026-07-27 19:51:08 +0000

    remove the treview

--- src/Components/Tree/index.js
+++ src/Components/Tree/index.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect, useRef, useContext } from "react";
 import SvgIcon from "@mui/material/SvgIcon";
 import TreeView from "@mui/lab/TreeView";
 import { SimpleTreeView } from '@mui/x-tree-view/SimpleTreeView';
-import { TreeItem } from '@mui/x-tree-view/TreeItem';
 import * as apiService from "services/apiService";
 import { Link, useParams } from "react-router-dom";
 import ContextMenu from "../ContextMenu";
@@ -202,23 +201,6 @@ export default function CustomizedTreeView({ setSelectedTreeNode, selectedTreeNo
   return (
     <div className="conainer" ref={containerRef}>
       <p>pawsel</p>
-      <SimpleTreeView>
-        <TreeItem itemId="grid" label="Data Grid">
-          <TreeItem itemId="grid-community" label="@mui/x-data-grid" />
-          <TreeItem itemId="grid-pro" label="@mui/x-data-grid-pro" />
-          <TreeItem itemId="grid-premium" label="@mui/x-data-grid-premium" />
-        </TreeItem>
-        <TreeItem itemId="pickers" label="Date and Time Pickers">
-          <TreeItem itemId="pickers-community" label="@mui/x-date-pickers" />
-          <TreeItem itemId="pickers-pro" label="@mui/x-date-pickers-pro" />
-        </TreeItem>
-        <TreeItem itemId="charts" label="Charts">
-          <TreeItem itemId="charts-community" label="@mui/x-charts" />
-        </TreeItem>
-        <TreeItem itemId="tree-view" label="Tree View">
-          <TreeItem itemId="tree-view-community" label="@mui/x-tree-view" />
-        </TreeItem>
-      </SimpleTreeView>
       treeview:
       <SimpleTreeView
         expanded={expanded}
--- src/Consts.js
+++ src/Consts.js
@@ -37,4 +37,4 @@ const prod={
   //  apiRoot : 'http://localhost:5002/'
 }
 
-export const config = process.env.NODE_ENV === 'development' ? dev : prod;
+export const config = process.env.NODE_ENV === 'development' ? prod : prod;
